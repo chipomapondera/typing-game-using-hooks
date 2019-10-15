49f63d53db9c0a3d8c5436f2349eb13c76fe4346
chipo mapondera <chipo.mapondera@ynap.com>
2019-10-15 17:47:51 +0100

    Typing game with snippet, input and game state functionality

--- src/App.js
+++ src/App.js
@@ -1,26 +1,66 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {useState, useEffect} from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+  const SNIPPETS = [
+    "Bears, beets, battlestar galactica",
+    "What's Forrest Gump's password? 1Forrest1",
+    "Where do programmers like to hangout? The Foo Bar"
+  ];
+
+  const initialGameState = {victory: false, startTime: null, endTime: null}
+
+  const [snippet, setSnippet] = useState('')
+  const [userText, setUserText] = useState('');
+  const [gameState, setGameState] = useState(initialGameState)
+
+  useEffect(() => {
+    if(gameState.victory) document.title = "Victory!";
+  });
+  
+  const updateUserText = (event) => {
+    event.preventDefault();
+
+    setUserText(event.target.value);
+    console.log('current userText', userText);
+
+    if (event.target.value === snippet) {
+      setGameState({
+        ...gameState,
+        victory: true,
+        endTime: new Date().getTime() - gameState.startTime
+      });
+    }
+  }
+
+  const chooseSnippet = snippetIndex => () => {
+    console.log('setSnippet', snippetIndex);
+    setSnippet(SNIPPETS[snippetIndex]);
+    setGameState({...gameState, startTime: new Date().getTime() });
+  };
+  
+    return (
+      <div>
+        <h2>Type Race</h2>
+        <hr />
+        <h3>Snippet</h3>
+        {snippet}
+        <h4>{gameState.victory ? `Done! 🎉 Time: ${gameState.endTime}ms` : null}</h4>
+        <input value={userText} onChange={updateUserText} />
+        <br/>
+        <br/>
+        <button>Start a new race!</button>
+        <br/>
+        <hr />
+        {
+          SNIPPETS.map((SNIPPET, index) => (
+            <button onClick={chooseSnippet(index)} key={index}>
+              {SNIPPET.substring(0, 10)}...
+            </button>
+          ))
+        }
+      </div>
+    )
+  }
+
 
 export default App;
